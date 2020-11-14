5d711ede51b0e98885b0c62342285a5ac3525297
Ertan Kara <kara.ertan9@gmail.com>
2020-11-14 16:38:07 +0300

    Add solution for powxn)

---
+++ problems/python/powxn.py
@@ -0,0 +1,19 @@
+from math import pow
+
+
+class Solution():
+    def myPow(_, x: float, n: int) -> float:
+        return pow(x, n)
+
+
+def check_solution():
+    solution = Solution()
+    delta = 0.0001
+    assert abs(solution.myPow(2.00000, 10) - 1024.00000 < delta)
+    assert abs(solution.myPow(2.10000, 3) - 9.26100 < delta)
+    assert abs(solution.myPow(2.00000, -2) - 2 < delta)
+    assert abs(solution.myPow(-2.00000, 2) - 4 < delta)
+    print("Correct!")
+
+
+check_solution()

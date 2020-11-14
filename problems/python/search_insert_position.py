43a62bda6338ad10e63c0055a16e31a62f2d3ae7
Ertan Kara <kara.ertan9@gmail.com>
2020-11-14 20:27:50 +0300

    Add another approach for for searchInsert

--- problems/python/search_insert_position.py
+++ problems/python/search_insert_position.py
@@ -17,12 +17,19 @@ class Solution:
 
         return len(nums)
 
+    def searchInsert2(_, nums: List[int], target: int) -> int:
+        for index, num in enumerate(nums):
+            if target <= num:
+                return index
+
+        return len(nums)
+
 
 def check_solution():
     solution = Solution()
-    assert solution.searchInsert([1, 2, 3], 4) == 3
-    assert solution.searchInsert([5, 10, 15, 25, 40], 35) == 4
-    assert solution.searchInsert([1, 2, 3, 4, 5], 2) == 1
+    assert solution.searchInsert2([1, 2, 3], 4) == 3
+    assert solution.searchInsert2([5, 10, 15, 25, 40], 35) == 4
+    assert solution.searchInsert2([1, 2, 3, 4, 5], 2) == 1
     print("Correct!")
 
 
